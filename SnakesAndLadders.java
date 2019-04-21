6d26ebed00b2db30e18bfc31a9dcf3703ec22d81
Mustafa Mossa <44642964+MMGit64@users.noreply.github.com>
2019-04-21 16:08:05 +0100

    Update SnakesAndLadders.java

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 
 
-public class SnakesAndLadders {
+public class SnakesAndLadders {													//Main method to start the game using the 'conditions' and 'start game' methods
 
 	public static void main(String[] args) {
 		SnakeLadderConditions snakeLadders = new SnakeLadderConditions();
@@ -17,9 +17,9 @@ public class SnakesAndLadders {
 
 
 
-class SnakeLadderConditions {
+class SnakeLadderConditions {																//Conditions to be set
 	
-	final static int WINPOINT = 100;
+	final static int WINPOINT = 100;														//100 is the winning point
 	
 	
 	static Map<Integer,Integer> snake = new HashMap<Integer,Integer>();
@@ -41,7 +41,7 @@ class SnakeLadderConditions {
 	
 	
 	
-	public int rollDice() {
+	public int rollDice() {														//Setting the dice to be randomised when rolled
 		int dice = 0;
 		
 		Random random = new Random();
@@ -52,306 +52,7 @@ class SnakeLadderConditions {
 	
 	
 	
-	public void startGame() {
-		
-		int userPlayer = 0, computer = 0;
-		//int currentPlayer;																	
-		Scanner scanner = new Scanner(System.in);
-		String roll;
-		int diceValue = 0;
-		
-		
-		System.out.println("Hello! Lets play rock, paper, scissors!");
-		
-
-		System.out.println("Enter your move:(0 = scissors; 1 = Rock; 2 = Paper):");
-		userPlayer = scanner.nextInt();
-		computer = scanner.nextInt();
-		
-		do {																		//**CHECK 'DO' FOR EACH IF/ELSE STATEMENTS**
-			
-			if (userPlayer < 0 || userPlayer > 2){
-				
-				System.out.println("Invalid choice");
-				System.exit(0);
-			}
-		
-
-			if(userPlayer == computer){
-
-				if(userPlayer == 0){
-
-					System.out.println("Tie game! You both chose scissors!");
-					System.exit(0);
-				}
-
-				else if (userPlayer == 1){
-
-					System.out.println("Tie game! You both chose rock!");
-					System.exit(0);
-				}
-
-				else if (userPlayer == 2){
-
-					System.out.println("Tie game! You both chose paper!");
-					System.exit(0);
-				}
-
-			}
-
-			if (userPlayer == 0){
-
-				if (computer == 1){
-
-					System.out.println("Player picked scissors! Computer picked rock!");
-					System.out.println("Computer Wins!");
-					int currentPlayer = 2;						
-
-					System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-					System.out.println("Press r to roll Dice");
-					roll = scanner.next();
-					diceValue = rollDice();
-
-
-					if(currentPlayer == 1) {
-						userPlayer = calculatePlayerValue(userPlayer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-
-						if(isWin(userPlayer)) {
-							System.out.println("FIRST PLAYER WINS!!!");
-							return;
-						}
-					
-					}
-					else {
-						computer = calculatePlayerValue(computer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-						if(isWin(computer))
-						{
-							System.out.println("SECOND PLAYER WINS!!!");
-							return;
-						}
-					} currentPlayer = -currentPlayer;					 
-				continue;
-				}
-
-				else if (computer == 2);{
-
-					System.out.println("Player picked scissors! Computer picked paper!");
-					System.out.println("Player Wins!");
-					int currentPlayer = 1;
-
-					System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-					System.out.println("Press r to roll Dice");
-					roll = scanner.next();
-					diceValue = rollDice();
-
-
-					if(currentPlayer == 1) {
-						userPlayer = calculatePlayerValue(userPlayer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-
-						if(isWin(userPlayer)) {
-							System.out.println("FIRST PLAYER WINS!!!");
-							return;
-						}
-					
-					}
-					else {
-						computer = calculatePlayerValue(computer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-						if(isWin(computer))
-						{
-							System.out.println("SECOND PLAYER WINS!!!");
-							return;
-						}
-					} currentPlayer = -currentPlayer;					 
-				continue;
-				}
-
-
-
-			}
-
-			if (userPlayer == 1){
-
-				if (computer == 0){
-
-					System.out.println("Player picked rock! Computer picked scissors!");
-					System.out.println("Player Wins!");
-					int currentPlayer = 1;	
-
-					System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-					System.out.println("Press r to roll Dice");
-					roll = scanner.next();
-					diceValue = rollDice();
-
-
-					if(currentPlayer == 1) {
-						userPlayer = calculatePlayerValue(userPlayer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-
-						if(isWin(userPlayer)) {
-							System.out.println("FIRST PLAYER WINS!!!");
-							return;
-						}
-					
-					}
-					else {
-						computer = calculatePlayerValue(computer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-						if(isWin(computer))
-						{
-							System.out.println("SECOND PLAYER WINS!!!");
-							return;
-						}
-					} currentPlayer = -currentPlayer;					 
-				continue;
-				}
-
-				else if (computer == 2);{
-
-					System.out.println("Player picked rock! Computer picked paper!");
-					System.out.println("Computer Wins!");
-					int currentPlayer = 2;
-
-
-					System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-					System.out.println("Press r to roll Dice");
-					roll = scanner.next();
-					diceValue = rollDice();
-
-
-					if(currentPlayer == 1) {
-						userPlayer = calculatePlayerValue(userPlayer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-
-						if(isWin(userPlayer)) {
-							System.out.println("FIRST PLAYER WINS!!!");
-							return;
-						}
-					}
-					else {
-						computer = calculatePlayerValue(computer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-						if(isWin(computer))
-						{
-							System.out.println("SECOND PLAYER WINS!!!");
-							return;
-						}
-					} currentPlayer = -currentPlayer;
-				continue;
-				}
-
-
-			}
-
-			if (userPlayer == 2){
-
-				if (computer == 0){
-
-					System.out.println("You picked paper! Computer picked scissors!");
-					System.out.println("Computer Wins!");
-					int currentPlayer = 2;
-
-					System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-					System.out.println("Press r to roll Dice");
-					roll = scanner.next();
-					diceValue = rollDice();
-
-
-					if(currentPlayer == 1) {
-						userPlayer = calculatePlayerValue(userPlayer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-
-						if(isWin(userPlayer)) {
-							System.out.println("FIRST PLAYER WINS!!!");
-							return;
-						}
-					
-					}
-					else {
-						computer = calculatePlayerValue(computer,diceValue);
-						System.out.println("First Player Score	: " + userPlayer);
-						System.out.println("Second Player Score	: " + computer);
-						System.out.println("------------------");
-						if(isWin(computer))
-						{
-							System.out.println("SECOND PLAYER WINS!!!");
-							return;
-						}
-					} currentPlayer = -currentPlayer;									//Allows switching of players
-
-				continue;
-				}
-			
-			}
-					 
-				
-			
-			else if (computer == 1);{
-					
-					System.out.println("You picked paper! Computer picked rock!");
-					System.out.println("Player Wins!");
-					 int currentPlayer = 1;
-				
-					 
-			System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
-			System.out.println("Press r to roll Dice");
-			roll = scanner.next();
-			diceValue = rollDice();
-			
-			
-			if(currentPlayer == 1) {
-				userPlayer = calculatePlayerValue(userPlayer,diceValue);
-				System.out.println("First Player Score	: " + userPlayer);
-				System.out.println("Second Player Score	: " + computer);
-				System.out.println("------------------");
-				
-				if(isWin(userPlayer)) {
-					System.out.println("FIRST PLAYER WINS!!!");
-					return;
-				}
-			}
-			else {
-				computer = calculatePlayerValue(computer,diceValue);
-				System.out.println("First Player Score	: " + userPlayer);
-				System.out.println("Second Player Score	: " + computer);
-				System.out.println("------------------");
-				if(isWin(computer))
-				{
-					System.out.println("SECOND PLAYER WINS!!!");
-					return;
-				}
-			} currentPlayer = -currentPlayer;									//Allows switching of players
-		continue;	
-		}
-	
-	  }while("r".equals(roll));
-
-   }	
-	
-	
-	
-	public int calculatePlayerValue(int player, int diceNum) {
+	public int calculatePlayerValue(int player, int diceNum) {						// Defines what happens when a player lands a snake or ladder position.
 		player = player + diceNum;
 		
 		if(player > WINPOINT)
@@ -373,9 +74,168 @@ class SnakeLadderConditions {
 		}
 		return player;
 	}
-	
-	public boolean isWin(int player) {
+
+
+	public boolean isWin(int player) {												//Sets up the winning position referring back to 'WINPOINT' in 'snakes&laddersCondition' method
 		return WINPOINT == player;
 	}
+
+
+	public int rockPaperScissors(int Player) {
+
+		System.out.println("Hello! Lets play rock, paper, scissors!");
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int Player1, Player2;
+
+		System.out.println("Enter your move:(0 = scissors; 1 = Rock; 2 = Paper):");
+		Player1 = scanner.nextInt();
+		Player2 = scanner.nextInt();
+
+		if (Player1 < 0 || Player1 > 2 || Player2 < 0 || Player2 > 2){
+
+			System.out.println("Invalid choice");
+			System.exit(0);
+		}
+
+
+		if(Player1 == Player2){
+
+			if(Player1 == 0){
+
+				System.out.println("Tie game! You both chose scissors!");
+			}
+
+			else if (Player1 == 1){
+
+				System.out.println("Tie game! You both chose rock!");
+			}
+
+			else if (Player1 == 2){
+
+				System.out.println("Tie game! You both chose paper!");
+			}
+
+			System.exit(0);
+		}
+
+		if (Player1 == 0){
+
+			if (Player2 == 1){
+
+				System.out.println("First Player picked scissors! Second Player picked rock!");
+				System.out.println("Second Player Wins!");
+				Player = -1;
+
+			}
+
+			else if (Player2 == 2);{
+
+				System.out.println("First Player picked scissors! Second Player picked paper!");
+				System.out.println("First Player Wins!");
+				Player = 1;
+			}
+
+			//System.exit(0);
+
+		}
+
+		if (Player1 == 1){
+
+			if (Player2 == 0){
+
+				System.out.println("First Player picked rock! Second Player picked scissors!");
+				System.out.println("First Player Wins!");
+				Player = 1;
+
+			}
+
+			else if (Player2 == 2);{
+
+				System.out.println("First Player picked rock! Second Player picked paper!");
+				System.out.println("Second Player Wins!");
+				Player = -1;
+			}
+
+			//System.exit(0);
+		}
+
+		if (Player1 == 2){
+
+			if (Player2 == 0){
+
+				System.out.println("First Player picked paper! Second Player picked scissors!");
+				System.out.println("Second Player Wins!");
+				Player = -1;
+
+			}
+
+			else if (Player2 == 1);{
+
+				System.out.println("First Player picked paper! Second Player picked rock!");
+				System.out.println("First Player Wins!");
+				Player = 1;
+			}
+
+			
+		}
+		return Player;		
+	}
 	
+	
+	
+	
+	public void startGame() {															// 1) Starts the game							
+																						// 2) Ensure that as long as currentPlayer = 1, the first player will start
+		int FirstPlayer = 0, SecondPlayer = 0;											// 3) By pressing 'r' for roll, 'diceValue' will refer to the conditions of 'rollDice' method 																		
+		int currentPlayer = 0;																						
+		Scanner scanner = new Scanner(System.in);
+		String roll;
+		int diceValue = 0;
+		currentPlayer = rockPaperScissors(currentPlayer);
+		
+		do {
+			System.out.println(currentPlayer==1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
+			System.out.println("Press r to roll Dice");
+			roll = scanner.next();
+			diceValue = rollDice();
+			
+			if(currentPlayer == 1) {
+				FirstPlayer = calculatePlayerValue(FirstPlayer,diceValue);					// 4) Refers to 'calculatePlayerValue' method to determine players current positions
+				System.out.println("First Player Score	: " + FirstPlayer);
+				System.out.println("Second Player Score	: " + SecondPlayer);
+				System.out.println("------------------");
+				
+				if(isWin(FirstPlayer)) {													// 5) Refers to 'isWin' method that determines when a player reaches 100.
+					System.out.println("FIRST PLAYER WINS!!!");
+					return;
+				}
+			}
+			else {
+				SecondPlayer = calculatePlayerValue(SecondPlayer,diceValue);
+				System.out.println("First Player Score	: " + FirstPlayer);
+				System.out.println("Second Player Score	: " + SecondPlayer);
+				System.out.println("------------------");
+				
+				if(isWin(SecondPlayer)) {
+					System.out.println("SECOND PLAYER WINS!!!");
+					return;
+				}
+			}currentPlayer = -currentPlayer;												// 6) Allows switching of players
+			
+		}while("r".equals(roll));
+	
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
